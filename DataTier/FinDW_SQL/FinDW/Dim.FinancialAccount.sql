612e1b0304d31ebd93c546e405fe86ddba0a45f2
user <graham.crowell@gmail.com>
2016-11-15 19:30:50 -0800

    consolidate + biml

---
+++ DataTier/FinDW_SQL/FinDW/Dim.FinancialAccount.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [Dim].[FinancialAccount]
+(
+	[FinancialAccountID] INT IDENTITY(0,1), 
+    [FinancialMainAccount_Str] NVARCHAR(100) NULL, 
+    [FinancialSubAccount_Str] NVARCHAR(100) NULL, 
+    CONSTRAINT [PK_FinancialStatement] PRIMARY KEY ([FinancialAccountID]) 
+)
+
+GO
+
+
+CREATE INDEX [IX_FinancialAccount_Lkup] ON [Dim].[FinancialAccount] ([FinancialMainAccount_Str], [FinancialSubAccount_Str])
