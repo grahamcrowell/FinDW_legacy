25870e086120d2fe590b8f6cf5c7bc9cece3a060
Graham Crowell <graham.crowell@gmail.com>
2017-03-11 17:52:29 -0800

    clean up

--- DataTier/FinDW_SQL/SqlQuery_1.sql
+++
@@ -1,5 +0,0 @@
-﻿USE [master]
-RESTORE DATABASE [WideWorldImportersDW] FROM  DISK = N'C:\SQLbackups\WideWorldImportersDW-Full.bak' WITH  FILE = 1,  MOVE N'WWI_Primary' TO N'C:\SQL_Data\WideWorldImportersDW.mdf',  
-MOVE N'WWI_UserData' TO N'C:\SQL_Data\WideWorldImportersDW_UserData.ndf',  MOVE N'WWI_Log' TO N'C:\SQL_Logs\SQL2016\WideWorldImportersDW.ldf', 
-MOVE N'WWIDW_InMemory_Data_1' TO N'C:\SQL_Data\WideWorldImportersDW_InMemory_Data_1',  NOUNLOAD,  REPLACE,  STATS = 5
-GO
